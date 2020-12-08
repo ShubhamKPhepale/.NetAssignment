c1941340fcc237c72cd6bf60cf5d65a51c53a40f
ShubhamKPhepale <65208936+ShubhamKPhepale@users.noreply.github.com>
2020-12-08 21:51:48 +0530

    Assignment 5_3 is uploaded

---
+++ Program5_3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5_3
+{
+    class Program5_3
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(1, "shubham", "HR", 24);
+            Employee e2 = new Employee(2, "sandy", "Manager", 25);
+            List<Employee> list = new List<Employee>();
+
+            list.Add(e1);
+            list.Add(e2);
+
+
+
+            Employee[] e = list.ToArray();
+
+            foreach (Employee Emp in e)
+            {
+                Console.WriteLine(Emp);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        int empno;
+        string name;
+        string designation;
+        int age;
+
+        public Employee(int empno, string empname, string designation, int age)
+        {
+            this.empno = empno;
+            this.name = empname;
+            this.designation = designation;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return empno + " " + name + " " + designation + " " + age;
+        }
+    }
+
+}
