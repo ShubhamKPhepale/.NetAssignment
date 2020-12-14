0db7506cbaf7d85b1fb9982cca3a322ef5af5181
ShubhamKPhepale <65208936+ShubhamKPhepale@users.noreply.github.com>
2020-12-14 11:52:46 +0530

    Assignment 6_4 is uploaded

---
+++ Program6_4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6_4
+{
+    class Program6_4
+    {
+        public delegate bool IsEven(int num);
+        static void Main(string[] args)
+        {
+            IsEven e = (num) =>
+            {
+
+                return num % 2 == 0;
+
+            };
+
+            Console.WriteLine("Enter number : ");
+
+            int no = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("IsEven : " + e(no));
+
+            Console.ReadLine();
+        }
+    }
+}
