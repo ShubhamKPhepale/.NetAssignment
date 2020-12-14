9044644e1316802b977e678358060a1b5fab9264
ShubhamKPhepale <65208936+ShubhamKPhepale@users.noreply.github.com>
2020-12-14 11:52:10 +0530

    Assignment 6_3 is uploaded

---
+++ Program6_3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6_3
+{
+    class Program6_3
+    {
+        public delegate decimal GetBasic(decimal basic);
+        static void Main(string[] args)
+        {
+            GetBasic g = (basic) =>
+            {
+                return basic;
+            };
+
+            Console.WriteLine("Basic salary is : " + g(25000));
+
+            Console.ReadLine();
+        }
+    }
+}
