77f0c5b71af2a7577cb59057f9a9e111310a7221
ShubhamKPhepale <65208936+ShubhamKPhepale@users.noreply.github.com>
2020-12-14 11:53:20 +0530

    Assignment 6_5 is uploaded

---
+++ Program6_5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignemnt6_5
+{
+    class Program6_5
+    {
+        public delegate bool IsGreaterThan(decimal basic);
+        static void Main(string[] args)
+        {
+            IsGreaterThan g = (basic) =>
+            {
+                if (basic > 10000)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            };
+
+            Console.WriteLine("Enter Basic : ");
+
+            int Basic = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("IsGreaterThan10000 : " + g(Basic));
+
+            Console.ReadLine();
+        }
+    }
+}
