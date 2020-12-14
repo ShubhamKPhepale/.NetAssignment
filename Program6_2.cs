4fbab8b595d2ffbacf323c6f9fd65a94c8449210
ShubhamKPhepale <65208936+ShubhamKPhepale@users.noreply.github.com>
2020-12-14 11:51:44 +0530

    Assignment 6_2 is uploaded

---
+++ Program6_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assignment6_2
+{
+    class Program6_2
+    {
+        public delegate bool isGreater(int a, int b);
+        static void Main(string[] args)
+        {
+            isGreater g1 = (a, b) =>
+            {
+
+                if (a > b)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            };
+
+            Console.WriteLine(g1(20, 10));
+
+            Console.ReadLine();
+
+        }
+    }
+}
